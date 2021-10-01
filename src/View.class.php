3f8c0f45a5ac73548367f2a36dce23917c91ef14
z3in <sebaschianrobin@gmail.com>
2021-10-01 19:09:34 +0800

    changes on the php routing

--- src/View.class.php
+++ src/View.class.php
@@ -3,21 +3,21 @@
 Class View {
     public static function dashboardDisplay(){
         $path = ROOT . DS . 'view'. DS . 'dashboard '. DS . 'index.html';
-        include($path);
+        require_once($path);
     }
 
     public static function dashboardLogin(){
         $path = ROOT . DS . 'view'. DS . 'dashboard '. DS . 'secure' . DS . 'login.html';
-        include($path);
+        require_once($path);
     }
 
     public static function webClientDisplay(){
         $path = ROOT . DS . 'view'. DS . 'web ' . DS . 'index.html';
-        include($path);
+        require_once($path);
     }
 
     public static function get404(){
         $path = ROOT . DS . '404.html';
-        include($path);
+        require_once($path);
     }
 }
